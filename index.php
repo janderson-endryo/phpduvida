93f9cd31243938b086bae1d9a37343d3d7bad04c
janderson endryo <102618938+janderson-endryo@users.noreply.github.com>
2024-02-05 11:48:23 -0300

    if

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,7 +23,37 @@ echo $number . " so vai adiciona aqui pois chamei novamente". "<br>";
 echo --$number . " decremento mesma regra <br>";
 
 $result = 50 == "50";
-var_dump($result);
+var_dump($result). "<br>";
+echo "<br>";
+// ternario
+
+$n1 = 100;
+$n2 = 20;
+
+$resultado = $n1 > $n2;
+
+echo ($resultado) ? 'e verdadeiro' : 'e falso';
+echo '<br>';
+if ($result) {
+    echo "e verdadeiro";
+} else {
+    echo 'e falso';
+}
+
+echo '<br>';
+
+if ($result) {
+    echo 'e verdadeiro';
+    // return; // o codigo para aqui e nao vai roda o codigo a baixo
+} 
+
+
+
+
+
+
+
+
 ?>
 
 </body>
