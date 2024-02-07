623d55a9f2572cd5edefd868344a37f93088c0e3
janderson endryo <102618938+janderson-endryo@users.noreply.github.com>
2024-02-07 11:21:27 -0300

    array e looping

--- index.php
+++ index.php
@@ -47,11 +47,45 @@ if ($result) {
     // return; // o codigo para aqui e nao vai roda o codigo a baixo
 } 
 
+echo '<br>';
+// array
+$names = [ 'janderson','endryo','alves', 
+"numeros" => [10,11,12],
+];
 
+echo $names[1];
+echo '<br>';
+echo var_dump($names['numeros']);
+echo '<br>';
+print_r($names);
+echo '<br>';
+// looping for com array 
 
+$array = ['jj','ee','aa','vv'];
 
+for($i = 0; $i < count($array); $i++) {
+    echo $array[$i]. "<br>";
+}
 
+// while com array
+$a = 0; 
+while($a < count($array)) {
+    echo $array[$a].'<br>';
+    $a++;
+}
 
+// do while com array
+$i = 0;
+do {
+    echo $array[$i] . "<br>";
+    $i++;
+} while ($i < count($array));
+
+// foreach com array
+
+foreach ($names['numeros'] as $key => $n) {
+    echo $key." " . $n . "<br>";
+}
 
 
 ?>
